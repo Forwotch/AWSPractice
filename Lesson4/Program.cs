23349b1486e6aa3d49ff0b14465de7fd3654aced
Oleksii Zemlianoi <Oleksii.Zemlianoi@refinitiv.com>
2022-02-06 22:57:36 +0200

    Add Lesson 4

---
+++ Lesson4/Program.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.EC2;
+
+Console.WriteLine("aws ec2 describe-instances");
+
+var ec2 = new AmazonEC2Client();
+
+var response = await ec2.DescribeInstancesAsync();
+Console.WriteLine($"Response status: {response.HttpStatusCode}");
+
+var numberOfReservations = response.Reservations.Count;
+Console.WriteLine($"Number of reservations: {numberOfReservations}");
+Console.WriteLine();
+
+Console.WriteLine("Instances description:");
+
+if (numberOfReservations > 0)
+{
+    foreach (var reservation in response.Reservations)
+    {
+        foreach (var instance in reservation.Instances)
+        {
+            var name = string.Empty;
+            foreach (var tag in instance.Tags)
+            {
+                if (tag.Key == "Name")
+                {
+                    name = tag.Value;
+                }
+            }
+
+            Console.WriteLine($"Name: {name}, IP: {instance.PublicIpAddress}");
+        }
+    }
+}
